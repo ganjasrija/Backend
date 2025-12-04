247ed0715e886ff57992dac872bd4d9fa7ff0149
ganjasrija <ganjasrija@gmail.com>
2025-12-04 10:39:32 +0530

    Initial commit

---
+++ Callback.js
@@ -0,0 +1,17 @@
+function getUserDetails(id,getSubjectDetails){
+    console.log("Getting rollnumber by id",id);
+    getSubjectDetails("123");
+
+}
+function getSubjectDetails(userroll,getSubjectMarks){
+         console.log("Getting subjectid by rollnumber",userroll);
+         getSubjectMarks("science-1");
+}
+function getSubjectMarks(subjectid){
+ console.log("Getting subject marks by subjectid",subjectid);
+}
+getUserDetails("121",function(userroll){
+    getSubjectDetails(userroll,function(subjectid){
+        getSubjectMarks(subjectid)
+    })
+})
